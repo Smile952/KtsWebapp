21e9eecc21f90a232f003fe337c787b008632fd5
Smile952 <deniska1234denis@gamil.com>
2025-02-26 20:00:08 +0700

    Adding entity framwork to project

---
+++ Core/Context/ApplicateionContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    class ApplicateionContext : DbContext
+    {
+        public ApplicateionContext(DbContextOptions<ApplicateionContext> context) : base(context)
+        {
+
+        }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(x => x.Id);
+            modelBuilder.Entity<Order>().HasKey(x => x.Id);
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<Employee>().HasMany(x => x.Orders).WithOne(x => x.Employee);
+            modelBuilder.Entity<User>().HasMany(x => x.Orders).WithOne(x => x.User);
+        }
+    }
+}
