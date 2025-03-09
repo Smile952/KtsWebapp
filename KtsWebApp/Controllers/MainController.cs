e24f9c463e6c0bcd0a55265c5ef23e70d0a9767f
Smile952 <deniska1234denis@gamil.com>
2025-03-09 12:33:19 +0700

    Fix some null warnings

--- KtsWebApp/Controllers/MainController.cs
+++ KtsWebApp/Controllers/MainController.cs
@@ -20,9 +20,9 @@ namespace Interface.Controllers
 
         public IActionResult RequestHandler([FromKeyedServices("service")] OrderService service)
         {
-            int dev = Int32.Parse(Request.Form["DevType"]);
+            int dev = Int32.Parse((string?)Request.Form["DevType"] ?? "0");
             string? content = Request.Form["content"];
-            service.Create(new RequestDTO() { EmployeeId = 1, userId = 1, OrderTypeId = dev, OrderContent = content });
+            service.Create(new RequestDTO() { EmployeeId = 1, userId = 1, OrderTypeId = dev, OrderContent = content ?? ""});
             return Redirect("/Main/DevTypes");
         }
     }
