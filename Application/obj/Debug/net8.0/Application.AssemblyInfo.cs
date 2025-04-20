98085745852ccf6224c452f95f13ec193f99074d
Smile952 <deniska1234denis@gamil.com>
2025-04-20 14:45:22 +0700

    trash commit

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f592fc8d16cfc8071de4db6c42ca5fbbe5ca75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b898e68b515692e6a519109cfc1745820989f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
