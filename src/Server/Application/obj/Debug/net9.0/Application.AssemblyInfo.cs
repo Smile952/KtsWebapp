cbb829a6eb5fd2c33fb87873fb9c213c913677d0
root <deniska1234denis@gamil.com>
2025-11-02 12:06:05 +0700

    Update version

--- src/Server/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Server/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b429a08062ccf4844d6e38deb5502298077d04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bad6b8564a2e243a9afc6d1928cc2f5107d42f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
