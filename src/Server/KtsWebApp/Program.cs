4359fed6eb7eca49d05ee64792ac9dd9bea22b60
Денис <kheragacy@MacBook-Air-Denis.local>
2025-12-22 16:12:23 +0700

    editing gitignore

--- src/Server/KtsWebApp/Program.cs
+++ src/Server/KtsWebApp/Program.cs
@@ -1,48 +1,12 @@
-using Application.Services;
-using Core.ApplicationContext;
-using Core.Repository;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using DotNetEnv;
-using Interface.Providers;
-using Microsoft.EntityFrameworkCore;
-using Interface;
-using System.Reflection.Metadata.Ecma335;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        string directory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
 
-        string envPath = Path.Combine(directory, ".env");
-        string token, addr, db_addr, db_user, db_password = "";
-        
-        if (File.Exists(envPath))
-        {
-            Env.Load(directory + "/.env");
-            token = Env.GetString("TOKEN");
-            addr = Env.GetString("LISTENING_ADDR");
-
-            db_addr = Env.GetString("DATABASE_ADDR");
-            db_user = Env.GetString("DATABASE_USER");
-            db_password = Env.GetString("DATABASE_PASSWORD");
-        }
-        else
-        {
-            token = Environment.GetEnvironmentVariable("TOKEN");
-            addr = Environment.GetEnvironmentVariable("LISTENING_ADDR");
-
-            db_addr = Environment.GetEnvironmentVariable("DATABASE_ADDR");
-            db_user = Environment.GetEnvironmentVariable("DATABASE_USER");
-            db_password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
-        }
-        
         var cors = "_myAllowSpecificOrigins";
         var builder = WebApplication.CreateBuilder(args);
-
-        string connectionString = $"Server={db_addr},1433;TrustServerCertificate=true;Database=KTS;User Id=sa;Password=1qaz@WSX";
 
         builder.Services.AddCors(options =>
         {
@@ -62,16 +26,16 @@ internal class Program
                 ValidateIssuer = false,
                 ValidateAudience = false,
                 ValidateLifetime = true,
-                ValidateIssuerSigningKey = true, 
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(token)) 
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("53db4fdf8da5212b2896c62a738794c35fb9ba73c8b6d9adc73a489ba1241149e1f569a2e244c244e8a5ae6b5de5fea6c12e4c190b9c0178b274a52e0bf62680"))
             };
         });
 
         builder.Services.AddAuthorization();
-        
+
         builder.Services.AddScoped<TokenProvider>();
 
-        builder.Services.AddScoped(typeof(Context), provider => new Context(connectionString));
+        builder.Services.AddScoped(typeof(Context), provider => new Context());
 
         builder.Services.AddScoped<RedisHandler>();
 
@@ -89,12 +53,10 @@ internal class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        builder.WebHost.UseUrls(addr);
-
         var app = builder.Build();
 
         app.UseCors(cors);
-        
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -111,7 +73,7 @@ internal class Program
             endpoint.MapControllers();
         });
 
-        
+
         app.Run();
     }
 }
